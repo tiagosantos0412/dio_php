6b5f228887436afc1b52b5171356e2bff62847d8
Tiago Felipe dos Santos <tiago.santos@unihosp.local>
2024-01-05 16:36:28 -0300

    first commit

---
+++ Introdução ao Ecossistema e Documentação PHP/index.php
@@ -0,0 +1,12 @@
+<?php
+    $saudacao =  "<h1>DIO. Digital Innovation One</h1>";
+    echo $saudacao;
+    $cursos = 30;
+    $pagina = "php";
+
+    if($pagina === "php"){
+        $cursos = 15;
+        echo "Temos $cursos disponíveis!";
+    }
+
+?>
